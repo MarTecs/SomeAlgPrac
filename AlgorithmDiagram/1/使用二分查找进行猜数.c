98760cbc1d8432d26c4d2e62be2bc13ce0eec37e
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-13 11:41:39 +0800

    add

---
+++ AlgorithmDiagram/1/使用二分查找进行猜数.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	
+	binary_search(5);
+	
+	int find = generatew_random(); 
+	
+} 
+
+/*
+	a[]为要找的数组
+	find 为要找的数
+	inpu 为自己输入的数 
+*/
+int binary_search(int a[], int find, int inpu) {
+	
+	printf("%d\n",a);
+	
+}
+
+/*
+	随机生成一个整数(要找的数)，并返回 
+*/
+int generate_random() {
+	return Math.floor(Math.random() * 101);
+}
