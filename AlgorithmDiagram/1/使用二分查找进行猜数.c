30a31b7443396c9f7113a4371c2955e560c372c5
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-13 11:58:11 +0800

    add ifles

--- AlgorithmDiagram/1/使用二分查找进行猜数.c
+++ AlgorithmDiagram/1/使用二分查找进行猜数.c
@@ -1,30 +1,57 @@
 #include<stdio.h>
-#include<math.h>
-#include<time.h>
 
-int main() {
+#include<math.h>
+
+#include <time.h> //用到了time函数 
+
+
+
+
+
+int main()
+{  
+	int find = generate_random();
 	
-	srand((unsigned) time(NULL));
-	int find = generate_random(); 
+   	int a[100];
+   	for ( int i = 0; i < 100; i++ ) {
+   		a[i] = i;
+	}
 	
-	printf("随机生成的一个整数为%d\n", find);
+	int inpu;
+	while( (input = getchar() ) != null ) {
+		binary_search(a, find, inpu);
+	}
 	
-} 
+  	return 0;
+}
+
+
 
 /*
-	a[]为要找的数组
 	find 为要找的数
 	inpu 为自己输入的数 
 */
-int binary_search(int a[], int find, int inpu) {
+void binary_search(int a[], int find, int inpu) {
 	
-	printf("%d\n",a);
+	int low = a[0];
+	int high = sizeof(a) / 4 - 1;
+	
+	for ( int i = low; i < high; i++ ) {
+		if ( a[i] > a[low]) {
+			
+		}
+	}
+	
+	
 	
 }
 
 /*
-	随机生成一个整数(要找的数)，并返回 
+	随机生成一个随机数，并进行返回 
 */
+
 int generate_random() {
-	return Math.floor(random() % 101);
+	srand((unsigned) time(NULL)); //用时间做种，每次产生随机数不一样
+	return rand() % 101;
 }
+
